11b62a925ded1d59068af2bb2555110f193d2b40
Dnbhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-11-28 16:50:03 -0600

    Loader feature component

---
+++ src/components/Loader.js
@@ -0,0 +1,18 @@
+import { ThreeDots } from "react-loader-spinner";
+
+const Loader = () => {
+  return (
+    <ThreeDots
+      height="80"
+      width="80"
+      color="maroon"
+      ariaLabel="tail-spin-loading"
+      radius="1"
+      wrapperStyle={{}}
+      wrapperClass=""
+      visible={true}
+    />
+  );
+};
+
+export default Loader;
