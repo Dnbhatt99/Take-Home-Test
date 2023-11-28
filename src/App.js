3a672762cb50c4512fe52ae9121a88760fd03550
Dnbhatt <62647174+Dnbhatt99@users.noreply.github.com>
2023-11-28 16:50:22 -0600

    added loader feature

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import "./style.scss";
 import { useState, useEffect } from "react";
 import Blogs from "./components/Blogs";
 import fetchblogs from "./WordpressAPI/api";
+import Loader from "./components/Loader";
 
 function App() {
   const [posts, setPosts] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     fetchblogs().then((res) => {
+      setIsLoading(false);
       setPosts(res);
     });
   }, []);
@@ -15,9 +18,18 @@ function App() {
   return (
     <div>
       <div className="row">
-        {posts.map((p) => (
-          <Blogs post={p} key={p.id} />
-        ))}
+        {isLoading ? (
+          <div
+            style={{
+              width: "100px",
+              margin: "auto",
+            }}
+          >
+            <Loader />
+          </div>
+        ) : (
+          posts.map((p) => <Blogs post={p} key={p.id} />)
+        )}
       </div>
     </div>
   );
--- src/WordpressAPI/api.js
+++ src/WordpressAPI/api.js
@@ -2,9 +2,14 @@ const url =
   "https://people.canonical.com/~anthonydillon/wp-json/wp/v2/posts.json";
 
 const fetchblogs = async () => {
-  const res = await fetch(url);
-  const data = await res.json();
-  return data;
+  try{
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  }
+  catch (err) {
+    console.log('There was an error', err);
+  }
 };
 
 export default fetchblogs;
